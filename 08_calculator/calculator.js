3a96ecf93ab534b839c71c18a90a3f7235e75607
Wyatt Brendle <accounts@wyattbrendle.tech>
2022-11-26 17:23:14 -0500

    Completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	const sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  const remaining = a - b;
+  return remaining;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let len = array.length;
+  let number = 0;
+  for(let i = 0; i < len; i++) {
+    number += array[i];
+  }
+
+  return parseInt(number);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let len = array.length;
+  let number = array[0];
+  for(let i = 1; i < len; i++) {
+    number *= array[i];
+  }
+  return parseInt(number);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  const exponent = a ** b;
+  return exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0){
+    return 1;
+  } else if (a < 0) {
+    return undefined;
+  } else {
+    let answer = a;
+    for(let i = a - 1; i > 0; i--) {
+      answer *= i;
+    }
+    return answer;
+  }    
 };
 
 // Do not edit below this line
