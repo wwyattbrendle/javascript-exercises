3d0d1cf1b2434108b3632c01f5e79763c6f9ff2e
Wyatt Brendle <accounts@wyattbrendle.tech>
2022-11-27 15:01:21 -0500

    Completed with a forloop and array.sort.  will check for a better solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const today = new Date();
+    for (let i = 0; i < array.length; i++) {
+        if (!array[i].yearOfDeath) array[i].yearOfDeath = today.getFullYear();
+    }
 
+    array.sort((a, b) => (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) ? 1 : -1)
+
+    return array[0];    
 };
 
 // Do not edit below this line
