46c9f83e2b2266b4f4c32d57a70be0a17d76db79
Wyatt Brendle <accounts@wyattbrendle.tech>
2022-11-27 12:25:20 -0500

    ompleted fibonacci with for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    const numbers = [];
+    for(let i = 0; i < n; i++) {
+        if (i === 0 || i === 1) {
+            numbers[i] = 1;
+            continue;
+        }
 
+        numbers[i] = numbers[i - 1] + numbers[i - 2];
+    }
+
+    return numbers[n - 1];
 };
 
 // Do not edit below this line
